f4b0acb7b0b747d4b86cfd4ea6405428158d00cd
Nico Krijnen <nico.krijnen@luminis.eu>
2023-06-11 14:44:59 +0200

    Restrict access as much as possible

--- src/main/kotlin/eu/luminis/workshop/cleancode/Volume.kt
+++ src/main/kotlin/eu/luminis/workshop/cleancode/Volume.kt
@@ -1,6 +1,6 @@
 package eu.luminis.workshop.cleancode
 
-class VolumeUnit(private val baseAmount: Int) {
+internal class VolumeUnit(private val baseAmount: Int) {
     companion object {
         val oz = VolumeUnit(1)
         val gill = VolumeUnit(5, oz)
@@ -9,10 +9,10 @@ class VolumeUnit(private val baseAmount: Int) {
 
     private constructor(amount: Int, unit: VolumeUnit) : this(amount * unit.baseAmount)
 
-    fun convertTo(other: VolumeUnit, amount: Number): Number = amount.toInt() * baseAmount / other.baseAmount
+    internal fun convertTo(other: VolumeUnit, amount: Number): Number = amount.toInt() * baseAmount / other.baseAmount
 }
 
-class Volume(private val amount: Number, private val unit: VolumeUnit) {
+class Volume internal constructor(private val amount: Number, private val unit: VolumeUnit) {
     override fun equals(other: Any?): Boolean {
         if (other !is Volume) return false
         return this.amount == other.unit.convertTo(this.unit, other.amount)
