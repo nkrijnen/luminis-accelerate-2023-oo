ad3916fba73bd21f51188d34b5ded53cea341e8e
Nico Krijnen <nico.krijnen@luminis.eu>
2023-06-11 21:26:43 +0200

    Make volume unit an inner class

--- src/main/java/eu/luminis/Volume.java
+++ src/main/java/eu/luminis/Volume.java
@@ -75,4 +75,30 @@ public class Volume {
     private boolean sameValue(Number value, Number other) {
         return Math.abs(value.doubleValue() - other.doubleValue()) < EPSILON;
     }
+
+    static class VolumeUnit {
+        static VolumeUnit oz = new VolumeUnit(1);
+        static VolumeUnit gill = new VolumeUnit(5, oz);
+        static VolumeUnit pint = new VolumeUnit(4, gill);
+        static VolumeUnit quart = new VolumeUnit(2, pint);
+        static VolumeUnit gallon = new VolumeUnit(4, quart);
+
+        private final int baseAmount;
+
+        VolumeUnit(int baseAmount) {
+            this.baseAmount = baseAmount;
+        }
+
+        VolumeUnit(int amount, VolumeUnit unit) {
+            this(amount * unit.baseAmount);
+        }
+
+        Number convertTo(VolumeUnit other, Number amount) {
+            return amount.doubleValue() * baseAmount / other.baseAmount;
+        }
+
+        Number convertToBaseUnit(Number amount) {
+            return amount.doubleValue() * baseAmount / oz.baseAmount;
+        }
+    }
 }
